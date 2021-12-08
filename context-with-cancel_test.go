820af71b53d888b28d923fcadeb1c1387fbcd579
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2021-12-08 09:14:30 +0700

    add context-with-cancel_test

---
+++ context-with-cancel_test.go
@@ -0,0 +1,52 @@
+package gocontext
+
+import (
+	"context"
+	"fmt"
+	"runtime"
+	"testing"
+	"time"
+)
+
+func CreateCounter(ctx context.Context) chan int {
+	destination := make(chan int)
+
+	go func() {
+		defer close(destination)
+		counter := 1
+		for {
+			select {
+			case <-ctx.Done():
+				return // stop goroutine and close 'destination' channel
+			default:
+				destination <- counter // send counter to destination
+				counter++
+			}
+		}
+	}()
+
+	return destination
+}
+
+func TestContextWithCancel(t *testing.T) {
+	fmt.Println("Total Goroutine:", runtime.NumGoroutine()) // default (goroutine = 2)
+
+	parent := context.Background()
+	ctx, cancel := context.WithCancel(parent)
+
+	destination := CreateCounter(ctx)
+	fmt.Println("Total Goroutine:", runtime.NumGoroutine())
+
+	for n := range destination {
+		fmt.Println("Counter:", n)
+		if n == 10 {
+			break
+		}
+	}
+
+	cancel() // send cancel signal to context, then context will done and stopped
+
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Total Goroutine:", runtime.NumGoroutine()) // goroutine leak (goroutine = 3) even loop is stoped (break)
+}
